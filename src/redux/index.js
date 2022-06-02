29d907caee4963df41558929abbf155bad437e9c
TheTarasik <athloon2002@gmail.com>
2022-06-02 19:04:30 +0300

    Clean

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,10 +1,8 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import main from './reducers/main';
 import menu from './reducers/menu';
 import todo from './reducers/todo';
 
 const rootReducer = combineReducers({
-     main,
      menu,
      todo
 });
